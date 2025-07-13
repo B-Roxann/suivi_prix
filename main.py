99e7470e2ebccb81c0d0ec6cf72ee807fb5989d1
Roxann BREMOND <roxannbremond@gmail.com>
2025-07-13 22:31:02 +0200

    Avancement sur le pseudo-code

--- main.py
+++ main.py
@@ -28,31 +28,28 @@ list_of_website = ["data/booky.html", "data/pagezen.html", "data/lireplus.html"]
 
 dictionnary_of_product = dict()
 
-for web_site in list_of_website:
-    with open(web_site, mode="r", encoding="utf-8") as file:
-        page_soup = BeautifulSoup(file, "html.parser")
+def data_exctract(web_site, ):
+    for web_site in list_of_website:
+        with open(web_site, mode="r", encoding="utf-8") as file:
+            page_soup = BeautifulSoup(file, "html.parser")
 
-        # On rajoute à notre dictionnaire les titres des sites scrapés
-        site = page_soup.title.string
-        dictionnary_of_product[site] = {}
+            # On rajoute à notre dictionnaire les titres des sites scrapés
+            site = page_soup.title.string
+            dictionnary_of_product[site] = {}
 
-        # On parcourt chaque produit et y récupère les informations nécessaire
-        product_list = page_soup.find_all("div", class_="produit")
-        list_of_product = dict()
+            # On parcourt chaque produit et y récupère les informations nécessaire
+            product_list = page_soup.find_all("div", class_="produit")
+            list_of_product = dict()
 
-        for product in product_list:
-            # On ajoute le nom du livre
-            name = product.find("h2").string
-            list_of_product[name] = {}
+            for product in product_list:
+                # On ajoute le nom du livre
+                name = product.find("h2").string
+                list_of_product[name] = {}
 
-            # A ce livre on ajoute son prix
-            price = product.find("p", class_="prix").get_text()
-            list_of_product[name]["prix :"] = price
+                # A ce livre on ajoute son prix
+                price = product.find("p", class_="prix").get_text()
+                list_of_product[name]["prix :"] = price
 
-            # Et sa description
-            description = product.find("p", class_="description").get_text()
-            list_of_product[name]["description :"] = description
-
-        # On ajoute notre liste de produits pour le site parcouru
-        dictionnary_of_product[site] = list_of_product
-        print(dictionnary_of_product)
+            # On ajoute notre liste de produits pour le site parcouru
+            dictionnary_of_product[site] = list_of_product
+            print(dictionnary_of_product)
